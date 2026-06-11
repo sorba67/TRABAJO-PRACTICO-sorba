3e54944b4b6939041433c396e251fdaea53cbcf0
sorba67 <50525455@ivt.edu.ar>
2026-06-11 09:53:47 -0300

    tp5
    hola

---
+++ tp5/app.js
@@ -0,0 +1,42 @@
+let numero1 = 50
+let numero2 = 55
+let suma = numero1 + numero2
+confirm("El resultado de la suma es: " + suma)
+let resta = numero1 - numero2
+confirm("El resultado de la resta es: " + resta)
+let multiplicacion = numero1 * numero2
+console.log("El resultado de la multiplicacion es: " + multiplicacion)
+let division = numero1 / numero2
+console.log("El resultado de la division es: " + division)
+
+
+
+let nombreAlumno = "bauti"
+let apellidoAlumno = "lopez"
+let edadAlumno = 14
+console.log("El alumno : " + nombreAlumno + " " + apellidoAlumno + " tiene " + edadAlumno + " años")
+
+
+
+
+let precio1 = 1300
+let precio2 = 2700
+let precio3 = 3000
+let total = precio1 + precio2 + precio3
+console.log("El total a pagar es : " + total)
+
+
+
+
+let comidaFavorita = "Cappeletis"
+let colorFavorito = "Negro"
+let numeroFavorito = 67
+console.log("Mi comida favorita es " + comidaFavorita + ", me gusta el color " + colorFavorito + " y mi número favorito es " + numeroFavorito)
+
+
+
+
+let pais = "argentina"
+console.log(pais)
+pais = "brasil"
+console.log(pais)
